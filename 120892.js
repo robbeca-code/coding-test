dedac003a1c06c64e4422ac6a4f184f880932b47
robbeca-code <79409722+robbeca-code@users.noreply.github.com>
2023-03-14 12:51:39 +0900

    Update 120892.js

--- 120892.js
+++ 120892.js
@@ -3,7 +3,7 @@ function solution(cipher, code) {
   
   // cipher의 인덱스를 code 값으로 나눴을 때 나머지가 0이면 code의 배수값이므로 answer에 삽입합니다.
   // i가 문자열이므로 *1을 해줘서 숫자 형태로 변환했습니다.
-  for(i in cipher) (i*1+1) % code === 0 ? answer += cipher[i] : null;
+  for(let i in cipher) (i*1+1) % code === 0 ? answer += cipher[i] : null;
   
   return answer;
 }
